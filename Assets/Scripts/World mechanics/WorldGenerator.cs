52b60ad773b2b4419a448fbc696d0ec345b3951d
CyberN00b <kpak2003@mail.ru>
2022-05-08 09:24:33 +0300

    optimazed buff spawner

--- Assets/Scripts/World mechanics/WorldGenerator.cs
+++ Assets/Scripts/World mechanics/WorldGenerator.cs
@@ -13,27 +13,32 @@ public class WorldGenerator : MonoBehaviour
     public float game_radius {
         get{return _game_radius;}
     }
+    private Controller controller = null;
     [SerializeField] private Bonus[] _bonusprefabs;
     private Bonus RandomBonus() => _bonusprefabs[UnityEngine.Random.Range(0, _bonusprefabs.Length)];
 
     private void Start()
     {
+        controller = GameObject.Find("GameController").GetComponent<Controller>();
         StartCoroutine("BonusGeneration");
     }
     void Update() {
 
     }
     void SpawnBonus(){
+        float tan = Mathf.Tan(Mathf.Deg2Rad * controller.angle);
         for(float i = -_game_radius; i < _game_radius; i += 0.5f)
         {
             for(float j = -_game_radius; j < _game_radius; j += 0.5f)
             {
                 float radius = j * j + i * i;
-                if (_game_radius * _game_radius > radius && radius > _player_radius * _player_radius)
-                    if (Random.Range(0, 1000) == 0)
+                if (_game_radius * _game_radius > radius && radius > _player_radius * _player_radius) {
+                    float angle = Mathf.Atan2(i, j) * Mathf.Rad2Deg - controller.angle;
+                    if (Random.Range(0, 1000) == 0 && angle > -90 && angle < 90)
                     {
                         Instantiate(RandomBonus(), new Vector3(i, 1f, j), Quaternion.identity, transform).is_prefab = false;
                     }
+                }
             }
         }
     }
