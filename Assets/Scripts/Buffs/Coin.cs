b3478574bd51d62146ef51f6ef517459769f55a5
dimon_torchila <dimaskivas42@gmail.com>
2022-05-22 21:25:05 +0300

    animations

--- Assets/Scripts/Buffs/Coin.cs
+++ Assets/Scripts/Buffs/Coin.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Coin : Bonus
 {
+    protected bool _isTriggered = false;
     void Awake() {
         _spawnY = 1.5f;
+        StartCoroutine(Anim());
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player") {
+            _isTriggered = true;
             Inventory inventory = other.GetComponent<Inventory>();
             inventory.ChangeCash(1);
-            Destroy(gameObject);
         }
+    }
+
+    private IEnumerator Anim()
+    {
+        while (!_isTriggered)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        gameObject.transform.GetChild(0).gameObject.GetComponent<Animation>().Play();
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
     }
 }
